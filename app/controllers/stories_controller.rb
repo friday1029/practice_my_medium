1a769edb165888aea2715f999bc8d90052aedb76
david <friday10292001@yahoo.com.tw>
2020-07-17 18:30:21 +0800

    finish unpublish 1.10

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -10,7 +10,8 @@ class StoriesController < ApplicationController
     end
     def create
         @story = current_user.stories.new(story_params)
-        @story.status = "published" if params[:publish]
+        # @story.status = "published" if params[:publish]
+        @story.publish! if params[:publish]
         if @story.save
             if params[:publish]
                 redirect_to stories_path, notice: "故事已發佈"
@@ -25,7 +26,16 @@ class StoriesController < ApplicationController
     end
     def update
         if @story.update(story_params)
-            redirect_to stories_path, notice: "故事更新成功"
+            case
+            when params[:publish]
+                @story.publish!
+                redirect_to stories_path, notice: '故事已發布'
+            when params[:unpublish]
+                @story.unpublish!
+                redirect_to stories_path, notice: '故事已下架'
+            else
+                redirect_to edit_story_path(@story), notice: '故事已儲存'
+            end
         else
             render :edit
         end
