fdf00c20020e802631c69ea4204aa9308a8e007d
David <friday10292001@yahoo.com.tw>
2020-08-05 16:18:45 +0800

    1.19C

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,29 @@
 class PagesController < ApplicationController
     def index
         # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
+        
         # 增加 where 去限制撈到的資料
         # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
+        
         # 使用scope
         # @stories = Story.published_stories.order(created_at: :desc).includes(:user)
+        
         # 因為使用了狀態機 (AASM), 有內建方法,就不用 scope
-        @stories = Story.published.order(created_at: :desc).includes(:user)
+        # @stories = Story.published.order(created_at: :desc).includes(:user)
+        
+        # Active Storage 把檔案都另外存,所以上面的語法會有 N+1 的問題
+        # with_attached_cover_image 是 Rails 內建的方法,可以順便把附檔一起讀出來
+        # with_attached_cover_image 的 cover_image 是附檔的名稱,
+        # 如果要抓 avatar,就是 with_attached_avatar
+        #@stories = Story.published.with_attached_cover_image.order(created_at: :desc).includes(:user)
+
+        #太長一串,丟到 scope 中
+        @stories = Story.published_stories
+
+
     end
+    
+    def show
+    end
+
 end
