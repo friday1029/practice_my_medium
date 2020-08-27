0d26d21f49a98950235e73f8299c04f816c71ec3
David <friday10292001@yahoo.com.tw>
2020-08-27 22:44:02 +0800

    paid user part1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+    def pricing
+
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: {
+    user: 0,
+    vip_user: 1,
+    platinum_user: 2,
+    admin: 3
+  }
+  
   # valitation
   validates :username, presence: true, uniqueness: true
 
@@ -13,6 +21,13 @@ class User < ApplicationRecord
   has_many :bookmarks
 
   # instance method
+  def paid_user?
+    # 使用了 enum 就會自動產生這樣的方法
+    vip_user? or platinum_user?
+    
+    # role == 1 or role == 2
+  end
+  
   def bookmark?(story)
     bookmarks.exists?(story: story)
   end
